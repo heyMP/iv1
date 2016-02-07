e169f13e43b6f706c5b6d36c56aab6145619c1f2
heyMP <mgp140@gmail.com>
2016-02-07 10:56:14 -0500

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+var viewportUnitsRepaint = require('./viewportUnitsRepaint.js');
+
+(function ($) {
+  'use strict';
+
+  objectFit.polyfill({
+      selector: 'img', // this can be any CSS selector
+      fittype: 'cover', // either contain, cover, fill or none
+      disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
+  });
+
+  // MenuToggle
+  if (typeof Drupal != 'undefined') {
+    Drupal.behaviors.repaintSlickbanner = {
+      attach: function (context, settings) {
+        viewportUnitsRepaint($('.slickheader__title'));
+      }
+    };
+  }
+  else {
+    $(document).ready(function() {
+      viewportUnitsRepaint($('.slickheader__title'));
+    });
+  }
+
+  // Vide video backgrounds
+  if (typeof Drupal != 'undefined') {
+    Drupal.behaviors.vide = {
+      attach: function (context, settings) {
+        // target any element with the data-vide attribute
+        $('*[data-vide]').each(function() {
+          // get the defined video name from the data-vide-src attribute
+          var videSrc = $(this).data('vide-src');
+          $(this).vide('../video/' + videSrc);
+        });
+      }
+    };
+  }
+  else {
+    $(document).ready(function() {
+      // target any element with the data-vide attribute
+      $('*[data-vide]').each(function() {
+        // get the defined video name from the data-vide-src attribute
+        var videSrc = $(this).data('vide-src');
+        $(this).vide('../video/' + videSrc);
+      });
+    });
+  }
+
+})(jQuery);
