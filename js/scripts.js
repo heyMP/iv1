437720a5d1099e258b5137ae80d84808ccc39813
heyMP <mgp140@gmail.com>
2016-02-12 17:50:35 -0500

    Eric changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,19 @@ var dialog = require('./components/dialog.js');
 (function ($) {
   'use strict';
 
-  objectFit.polyfill({
-      selector: 'img', // this can be any CSS selector
-      fittype: 'cover', // either contain, cover, fill or none
-      disableCrossDomain: 'true' // either 'true' or 'false' to not parse external CSS files.
-  });
+  // navigation
+  if (typeof Drupal != 'undefined') {
+    Drupal.behaviors.vide = {
+      attach: function (context, settings) {
+        $(".button-collapse").sideNav();
+      }
+    };
+  }
+  else {
+    $(document).ready(function() {
+      $(".button-collapse").sideNav();
+    });
+  }
 
   // Vide video backgrounds
   if (typeof Drupal != 'undefined') {
